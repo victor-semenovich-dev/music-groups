142f75a558b300749f8fc53b23812d3a7a3d98d1
Victor Semenovich <victor.semenovich@gmail.com>
2024-10-12 17:08:51 +0300

    configure firebase using flutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:music_groups/firebase_options.dart';
 import 'package:music_groups/ui/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MusicGroupsApp());
 }
