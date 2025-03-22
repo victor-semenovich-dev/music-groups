49cd20358ed6ca9939e807e4147049e76c573dc7
Victor Semenovich <victor.semenovich@gmail.com>
2025-03-22 10:24:24 +0300

    a blank groups route

---
+++ lib/ui/route/groups.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroupsRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Выбери группу'),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
