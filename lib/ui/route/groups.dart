6c8ccf7a8601183a2243d3b39f50d7fb55e0166f
Victor Semenovich <victor.semenovich@gmail.com>
2025-03-22 23:35:18 +0300

    selection a group route

--- lib/ui/route/groups.dart
+++ lib/ui/route/groups.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:music_groups/provider/groups.dart';
+import 'package:music_groups/ui/route/participation.dart';
+import 'package:provider/provider.dart';
 
 class GroupsRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Выбери группу'),
-      ),
-      body: SafeArea(
-        child: Container(),
-      ),
+    return ChangeNotifierProvider<GroupsProvider>(
+      create: (context) => GroupsProvider(),
+      child: Consumer<GroupsProvider>(builder: (context, provider, child) {
+        debugPrint("${provider.sortedGroups?.length} groups");
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Выбери группу'),
+          ),
+          body: SafeArea(
+            child: ListView(
+              children: (provider.sortedGroups ?? []).map((entry) {
+                return ListTile(
+                  title: Text(entry.value.name),
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              ParticipationRoute(groupId: entry.key)),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
